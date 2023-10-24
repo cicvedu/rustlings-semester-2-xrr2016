5435f7d826f58f3c2e9fc0f398cc14796124311f
冷石Boy <coldstoneboy@qq.com>
2023-10-24 17:58:12 +0000

    feat: update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,12 +32,27 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer<'a>(input: Vec<(String, Command)>) -> Vec<&'a str> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<&str> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {
+                    let o = &string[..];
+                    let s = String::from(o);
+                    &output.push(&s.as_str())
+                }
+                Command::Trim => &output.push(""),
+                Command::Append(n) => &{
+                    let o = &string[..];
+                    let mut s = String::from(o);
+                    s.push_str("bar".repeat(*n).as_str());
+                    &output.push("")
+                },
+            };
         }
+
         output
     }
 }
@@ -45,7 +60,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
@@ -56,6 +71,7 @@ mod tests {
             ("foo".into(), Command::Append(1)),
             ("bar".into(), Command::Append(5)),
         ]);
+
         assert_eq!(output[0], "HELLO");
         assert_eq!(output[1], "all roads lead to rome!");
         assert_eq!(output[2], "foobar");
