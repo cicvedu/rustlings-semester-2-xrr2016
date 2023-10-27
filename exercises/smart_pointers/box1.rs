dc681e570bd8411c131a75cc5cf14c9e68864df8
xrr2016 <xiaoranran1993@outlook.com>
2023-10-27 15:57:50 +0800

    feat: box1

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,9 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -34,12 +32,17 @@ fn main() {
     );
 }
 
+use crate::List::{Cons, Nil};
+
 pub fn create_empty_list() -> List {
-    todo!()
+    let list = List::Nil;
+    list
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    list
 }
 
 #[cfg(test)]
