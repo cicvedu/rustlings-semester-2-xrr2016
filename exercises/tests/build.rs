94c780fd9040ba545d2768bb85636e0e058097e2
xrr2016 <xiaoranran1993@outlook.com>
2023-10-30 15:05:27 +0800

    feat: as_ref_mut

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,26 +1,23 @@
 //! This is the build script for both tests7 and tests8.
 //!
 //! You should modify this file to make both exercises pass.
-use std::env;
-
 fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
     // Cargo do it.
+
     let timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
-    env::set_var("TEST_FOO", format!("{}", timestamp));
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
+    let your_command = format!("TEST_FOO={}", timestamp);
+    println!("your_command: {}", your_command);
+
     println!("cargo:{}", your_command);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
+    let your_command = "ustc-cfg=CFG";
     println!("cargo:{}", your_command);
 }
